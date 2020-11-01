5eb48151b4e975ade48cc89ddd7a72e010b81ef0
Riku-1 <osaka.riku@gmail.com>
2020-11-01 22:56:35 +0900

    feature(#11):usecaseを使うようにmain.pyを変更

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from usecase.scrape import scrape
+from usecase.scrape.scrape_usecase_factory import get_scrape_usecase
+
 
 # TODO: インプットの方式を考える
-url = "https://www.nature.com/articles/s41467-020-18002-w"
-thesis = scrape.get_thesis(url)
+url = "https://www.nature.com/articles/s41467-020-19293-9"
+
+scrape_usecase = get_scrape_usecase(url)
+thesis = scrape_usecase.get_thesis()
 print(thesis)
