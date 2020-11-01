95242e5fc74910e8e2cbd18e21b70305a169e2cd
Riku-1 <osaka.riku@gmail.com>
2020-11-01 19:09:55 +0900

    feature:domainクラスを使用するように変更

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,10 +1,16 @@
 import urllib3
 from bs4 import BeautifulSoup
 
+from domain import Thesis
 import formatting
 
 
-def get_thesis(url: str):
+def get_thesis(url: str) -> Thesis.Thesis:
+    """
+    論文を取得する
+    :param url:
+    :return:
+    """
     # header偽装
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
@@ -26,8 +32,9 @@ def get_thesis(url: str):
     for intro in introductions:
         _introductions += intro.get_text()
 
-    _abstract = formatting.delete_brackets(abstract)
-    _introduction = formatting.delete_brackets(introduction)
+    return Thesis.Thesis(
+        formatting.delete_brackets(abstract),
+        formatting.delete_brackets(_introductions)
+    )
 
-    return _abstract
 
