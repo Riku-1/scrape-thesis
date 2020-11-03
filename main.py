3282ac7e754598f962b4d022a1b8d631b88034fb
Riku-1 <osaka.riku@gmail.com>
2020-11-03 10:19:16 +0900

    feature(#24):url非対応時、ログにwarningを残すように変更

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ url = "https://www.nature.com/articles/s41467-020-19293-9"
 try:
     scrape_usecase = get_scrape_usecase(url)
 except RuntimeError as err:
+    logger.warning(err)
     print(err)
 
 thesis = scrape_usecase.get_thesis()
