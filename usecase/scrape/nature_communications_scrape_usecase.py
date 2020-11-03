1960a5af6d287afc1cd0c96800df75f46e5780fe
Riku-1 <osaka.riku@gmail.com>
2020-11-03 11:25:14 +0900

    feature(#26):セクション取得メソッド失敗時の処理を追加

--- usecase/scrape/nature_communications_scrape_usecase.py
+++ usecase/scrape/nature_communications_scrape_usecase.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from lxml import html
 
+import logger
 from usecase.scrape.scrape_usecase import ScrapeUseCase
 
 
@@ -15,8 +16,15 @@ def _get_section(soup: BeautifulSoup, section_name: str) -> str:
     """
     dom: html.HtmlElement = html.fromstring(str(soup))
     # textがセクション名（AbstractやIntroduction）の名前のh2要素を取得する（見出し）
-    title_id = dom.xpath(f"//h2[text()='{section_name}']")[0].get("id")
+    section_name_dom = dom.xpath(f"//h2[text()='{section_name}']")
+    if not section_name_dom:
+        warning_message = f"{section_name}が取得できませんでした。"
+        logger.warning(warning_message)
+        print(warning_message)
+        return ""
+
     # 見出しのidに-contentを加えたものが各セクションの内容のidになるので、これを取得する
+    title_id = section_name_dom[0].get("id")
     section_id = title_id + "-content"
 
     contents = soup.select(f"#{section_id} > p")
