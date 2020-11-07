d84d33d6265af81cdbab2eb3421b53673b4b967e
Riku-1 <osaka.riku@gmail.com>
2020-11-07 16:59:15 +0900

    feature(#44):DeepL APIを用いた翻訳メソッドを追加

---
+++ infrastructure/web/translate.py
@@ -0,0 +1,27 @@
+import requests
+
+import setting
+
+DEEPL_TRANSLATE_URL = "https://api.deepl.com/v2/translate"
+
+
+def translate(text_list: [str], source_lang=""):
+    params = {
+        "auth_key": setting.DEEPL_API_KEY,
+        "text": text_list,
+        "target_lang": "JA",
+        "source_lang": source_lang,
+    }
+
+    res = requests.get(DEEPL_TRANSLATE_URL, params)
+
+    if res.status_code == 403:
+        raise Exception("DeepLの認証に失敗しました。")
+    if res.status_code == 456:
+        raise Exception("DeepLの翻訳上限に達しました。")
+
+    translated_list = []
+    for elm in res.json()["translations"]:
+        translated_list.append(elm["text"])
+
+    return translated_list
