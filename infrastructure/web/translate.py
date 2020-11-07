194f0a077ba2428e5ea3ca4450ba094ab502927e
Riku-1 <osaka.riku@gmail.com>
2020-11-07 18:07:05 +0900

    fix(#44):source_langが空は許可されていなかったのを修正

--- infrastructure/web/translate.py
+++ infrastructure/web/translate.py
@@ -5,11 +5,10 @@ import setting
 DEEPL_TRANSLATE_URL = "https://api.deepl.com/v2/translate"
 
 
-def translate(text_list: [str], source_lang="") -> [str]:
+def translate(text_list: [str]) -> [str]:
     """
     文章を翻訳する
     :param text_list:
-    :param source_lang:
     :return: 翻訳された文章のリスト
     NOTE: 返り値もtext_listの順番どおりになる
     > The parameter may be specified multiple times and translations
@@ -20,7 +19,6 @@ def translate(text_list: [str], source_lang="") -> [str]:
         "auth_key": setting.DEEPL_API_KEY,
         "text": text_list,
         "target_lang": "JA",
-        "source_lang": source_lang,
     }
 
     res = requests.get(DEEPL_TRANSLATE_URL, params)
@@ -33,6 +31,7 @@ def translate(text_list: [str], source_lang="") -> [str]:
         raise Exception("DeepL APIがビジー状態です。しばらく待ってから再度実行してください。")
     if res.status_code == 456:
         raise Exception("DeepLの翻訳上限に達しました。")
+    print(res.json())
 
     translated_list = []
     for elm in res.json()["translations"]:
