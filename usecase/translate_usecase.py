e83790e820d5724746e2d463bd8d37010cca6890
Riku-1 <osaka.riku@gmail.com>
2020-11-07 18:43:59 +0900

    feature(#44):例外処理追加

--- usecase/translate_usecase.py
+++ usecase/translate_usecase.py
@@ -1,5 +1,6 @@
 from domain.thesis import Thesis
 from infrastructure.web.translate import translate
+import logger
 
 
 def translate_thesis(thesis: Thesis) -> Thesis:
@@ -8,18 +9,23 @@ def translate_thesis(thesis: Thesis) -> Thesis:
     :param thesis:
     :return: 翻訳情報が入力された論文クラス
     """
-    translated_list = translate([
-        thesis.title,
-        thesis.abstract,
-        thesis.introduction,
-        thesis.results,
-        thesis.discussion
-    ])
+    try:
+        translated_list = translate([
+            thesis.title,
+            thesis.abstract,
+            thesis.introduction,
+            thesis.results,
+            thesis.discussion
+        ])
 
-    thesis.title_translated = translated_list[0]
-    thesis.abstract_translated = translated_list[1]
-    thesis.introduction_translated = translated_list[2]
-    thesis.results_translated = translated_list[3]
-    thesis.discussion_translated = translated_list[4]
+        thesis.title_translated = translated_list[0]
+        thesis.abstract_translated = translated_list[1]
+        thesis.introduction_translated = translated_list[2]
+        thesis.results_translated = translated_list[3]
+        thesis.discussion_translated = translated_list[4]
+    except Exception as err:
+        logger.warning(f"{thesis.url}の翻訳に失敗しました。", err)
+        # 翻訳に失敗した場合、その時点での論文情報を返す
+        return thesis
 
     return thesis
