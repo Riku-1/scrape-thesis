2fb00468c44016c95f4046ab82fc082cfb1a5c28
Riku-1 <osaka.riku@gmail.com>
2020-11-07 17:41:03 +0900

    refactor(#44):translateメソッドに返り値と注釈を追加

--- infrastructure/web/translate.py
+++ infrastructure/web/translate.py
@@ -5,7 +5,17 @@ import setting
 DEEPL_TRANSLATE_URL = "https://api.deepl.com/v2/translate"
 
 
-def translate(text_list: [str], source_lang=""):
+def translate(text_list: [str], source_lang="") -> [str]:
+    """
+    文章を翻訳する
+    :param text_list:
+    :param source_lang:
+    :return: 翻訳された文章のリスト
+    NOTE: 返り値もtext_listの順番どおりになる
+    > The parameter may be specified multiple times and translations
+        are returned in the same order as they are requested.
+    from https://www.deepl.com/docs-api/translating-text/
+    """
     params = {
         "auth_key": setting.DEEPL_API_KEY,
         "text": text_list,
